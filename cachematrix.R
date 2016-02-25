32c02c26baa54e5c0ea1e9c8a2f97e26ff0a5fa5
Prashant Kumar <prashantkpathak@yahoo.com>
2016-02-25 18:00:02 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,21 @@
-# makeCacheMatrix is a function that returns a list of functions
-# Its puspose is to store a martix and a cached value of the inverse of the 
-# matrix. Contains the following functions:
-# * setMatrix      set the value of a matrix
-# * getMatrix      get the value of a matrix
-# * cacheInverse   get the cahced value (inverse of the matrix)
-# * getInverse     get the cahced value (inverse of the matrix)
-#
-# Notes:
-# not sure how the "x = numeric()" part works in the argument list of the 
-# function, but it seems to be creating a variable "x" that is not reachable 
-# from the global environment, but is available in the environment of the 
-# makeCacheMatrix function
+## Functions that cache the inverse of a matrix
+##
+## Usage example:
+##
+## > source('cachematrix.R')
+## > m <- makeCacheMatrix(matrix(c(2, 0, 0, 2), c(2, 2)))
+## > cacheSolve(m)
+## [,1] [,2]
+## [1,]  0.5  0.0
+## [2,]  0.0  0.5
+
+## Create a special "matrix", which is a list containing
+## a function to
+##   - set the value of the matrix
+##   - get the value of the matrix
+##   - set the value of the inverse matrix
+##   - get the value of the inverse matrix
+
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
